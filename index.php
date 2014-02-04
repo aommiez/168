ce41d6343346201667209ad0c169f0eeb41519ff
aommiez <aommiez@gmail.com>
2014-02-04 17:07:13 +0700

    update design

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/168travel.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link href="js/bootstrap-select.min.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Playfair+Display:400italic' rel='stylesheet' type='text/css' />
     <noscript>
         <link rel="stylesheet" type="text/css" href="css/noscript.css" />
@@ -33,64 +34,67 @@
         <div id="ei-slider" class="ei-slider">
             <ul class="ei-slider-large">
                 <li>
-                    <img src="images/large/6.jpg" alt="image06"/>
+                    <img src="images/large/01.png" alt="image06"/>
                     <div class="ei-title">
                         <h2>Passionate</h2>
                         <h3>Seeker</h3>
                     </div>
                 </li>
                 <li>
-                    <img src="images/large/1.jpg" alt="image01" />
+                    <img src="images/large/02.png" alt="image01" />
                     <div class="ei-title">
                         <h2>Creative</h2>
                         <h3>Duet</h3>
                     </div>
                 </li>
                 <li>
-                    <img src="images/large/2.jpg" alt="image02" />
+                    <img src="images/large/03.png" alt="image02" />
                     <div class="ei-title">
                         <h2>Friendly</h2>
                         <h3>Devil</h3>
                     </div>
                 </li>
                 <li>
-                    <img src="images/large/3.jpg" alt="image03"/>
+                    <img src="images/large/04.png" alt="image03"/>
                     <div class="ei-title">
                         <h2>Tranquilent</h2>
                         <h3>Compatriot</h3>
                     </div>
                 </li>
                 <li>
-                    <img src="images/large/4.jpg" alt="image04"/>
+                    <img src="images/large/05.png" alt="image04"/>
                     <div class="ei-title">
                         <h2>Insecure</h2>
                         <h3>Hussler</h3>
                     </div>
                 </li>
                 <li>
-                    <img src="images/large/5.jpg" alt="image05"/>
+                    <img src="images/large/05.png" alt="image04"/>
                     <div class="ei-title">
-                        <h2>Loving</h2>
-                        <h3>Rebel</h3>
+                        <h2>Insecure</h2>
+                        <h3>Hussler</h3>
                     </div>
                 </li>
                 <li>
-                    <img src="images/large/7.jpg" alt="image07"/>
+                    <img src="images/large/05.png" alt="image04"/>
                     <div class="ei-title">
-                        <h2>Crazy</h2>
-                        <h3>Friend</h3>
+                        <h2>Insecure</h2>
+                        <h3>Hussler</h3>
                     </div>
                 </li>
+
             </ul><!-- ei-slider-large -->
             <ul class="ei-slider-thumbs">
                 <li class="ei-slider-element">Current</li>
-                <li><a href="#">Slide 6</a><img src="images/thumbs/6.jpg" alt="thumb06" /></li>
-                <li><a href="#">Slide 1</a><img src="images/thumbs/1.jpg" alt="thumb01" /></li>
-                <li><a href="#">Slide 2</a><img src="images/thumbs/2.jpg" alt="thumb02" /></li>
-                <li><a href="#">Slide 3</a><img src="images/thumbs/3.jpg" alt="thumb03" /></li>
-                <li><a href="#">Slide 4</a><img src="images/thumbs/4.jpg" alt="thumb04" /></li>
-                <li><a href="#">Slide 5</a><img src="images/thumbs/5.jpg" alt="thumb05" /></li>
-                <li><a href="#">Slide 7</a><img src="images/thumbs/7.jpg" alt="thumb07" /></li>
+
+                <li><a href="#">Slide 6</a><img src="images/thumbs/m01.png" alt="thumb06" /></li>
+                <li><a href="#">Slide 1</a><img src="images/thumbs/m02.png" alt="thumb01" /></li>
+                <li><a href="#">Slide 2</a><img src="images/thumbs/m03.png" alt="thumb02" /></li>
+                <li><a href="#">Slide 3</a><img src="images/thumbs/m04.png" alt="thumb03" /></li>
+                <li><a href="#">Slide 4</a><img src="images/thumbs/m05.png" alt="thumb04" /></li>
+                <li><a href="#">Slide 3</a><img src="images/thumbs/m04.png" alt="thumb03" /></li>
+                <li><a href="#">Slide 4</a><img src="images/thumbs/m05.png" alt="thumb04" /></li>
+
             </ul><!-- ei-slider-thumbs -->
         </div><!-- ei-slider -->
     </div>
@@ -113,12 +117,24 @@
             </ul>
         </div>
         </div>
-        <div id="mainContent">
-            <div>
-                filter block
+        <div id="mainContent" >
+            <div class="rightBlockTop">
+                <select class="selectpicker">
+                    <option>Mustard</option>
+                    <option>Ketchup</option>
+                    <option>Relish</option>
+                </select
             </div>
-            <div>
-                main content
+            <div class="rightBlock">
+                <ul>
+                    <li>test 1</li>
+                    <li>test 2</li>
+                    <li>test 3</li>
+                    <li>test 4</li>
+                    <li>test 5</li>
+                    <li>test 6</li>
+                    <li>test 7</li>
+                </ul>
             </div>
         </div>
     </div>
@@ -137,6 +153,7 @@
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
+<script src="js/bootstrap-select.min.js"></script>
 <script type="text/javascript" src="js/jquery.eislideshow.js"></script>
 <script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
 <script type="text/javascript">
@@ -147,6 +164,7 @@
             slideshow_interval	: 3000,
             titlesFactor		: 0
         });
+        $('.selectpicker').selectpicker();
     });
 </script>
 </body>
